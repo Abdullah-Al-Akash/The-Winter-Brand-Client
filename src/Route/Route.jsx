d8775d1035f9ced3cac49afc2add4856f8126469
Ashiqur Rahman Biplop <biplops22@gmail.com>
2023-11-05 22:44:53 +0600

    added some code

--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -1,6 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main/Main";
 import Home from "../Pages/Main/Home/Home";
+import About from "../Pages/Main/About/About";
+import Contact from "../Pages/Main/Contact/Contact";
+import Reviews from "../Pages/Main/Reviews/Reviews";
+import Faq from "../Pages/Main/FAQ/Faq";
+import Login from "../Pages/Main/Login/Login";
+import Products from "../Pages/Main/Products/Products";
+import Register from "../Pages/Main/Register/Register";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +17,34 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+      {
+        path: "/reviews",
+        element: <Reviews></Reviews>,
+      },
+      {
+        path: "/faq",
+        element: <Faq></Faq>,
+      },
+      {
+        path: "/products",
+        element: <Products></Products>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
       },
     ],
   },
---
+++ src/Sheard/Drawer/DrawerComponent.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Drawer from "react-modern-drawer";
+import { GrFormClose } from "react-icons/gr";
+
+//import styles 👇
+import "react-modern-drawer/dist/index.css";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+import { useContext } from "react";
+const DrawerComponent = () => {
+  const { isOpen, setIsOpen, toggleDrawer } = useContext(AuthContext);
+  return (
+    <Drawer
+      open={isOpen}
+      onClose={toggleDrawer}
+      direction="right"
+      size={window.innerWidth >= 768 ? "50%" : "100%"}
+      className="bla bla bla relative"
+    >
+      <button
+        className="absolute md:top-10 md:right-10 top-5 right-5 text-2xl"
+        onClick={toggleDrawer}
+      >
+        <GrFormClose />
+      </button>
+      <div className="text-5xl border h-full flex justify-center items-center">
+        Under Construction
+      </div>
+    </Drawer>
+  );
+};
+
+export default DrawerComponent;

4545b1855b45e591a5e3d07c09dfc7dd20b1930a
Abdullah Al Akash <aabdullahalakash@gmail.com>
2023-11-15 16:52:12 +0600

    Added My Order Page

--- src/Pages/Main/MyOrder/MyOrder.jsx
+++ src/Pages/Main/MyOrder/MyOrder.jsx
@@ -1,42 +1,39 @@
 import React from "react";
+import { FaDownload } from "react-icons/fa";
+
 
 const MyOrder = () => {
+  // Todo When My Order Zero Handle You have no order yet!
   return <div className="">
-    <h1 className="text-center text-xl font-extrabold p-3">
-      Your All Products
+    <h1 className="text-center text-xl font-extrabold p-3 my-4">
+      My Order
     </h1>
 
     <div className="overflow-x-auto">
-      <table className="table">
+      <table className="table w-[1280px] mx-auto">
         {/* head */}
         <thead className="">
           <tr className="">
-            <th className="text-center">#</th>
-            <th className="text-center">Product Image</th>
-            <th className="text-center">Product Name</th>
-
-            <th className="text-center ">Price</th>
-            <th className="text-center ">Discount Price</th>
-            <th className="text-center ">Available quantity</th>
-            <th className="text-center">Already Sell</th>
-            <th className="text-center">Product States</th>
-            <th className="text-center">Action</th>
+            <th className="text-center">Order Id</th>
+            <th className="text-center">Name</th>
+            <th className="text-center">email</th>
+            <th className="text-center">Address</th>
+            <th className="text-center">Order Date</th>
+            <th className="text-center ">Order Status</th>
+            <th className="text-center ">Invoice</th>
           </tr>
         </thead>
 
 
         <tr className="text-center">
-          <th></th>
-          <td>
-
-          </td>
-          <td>product_name </td>
-          <td>price </td>
-          <td>discount </td>
-          <td>quantity</td>
-          <td>already_sell</td>
-          <td>
-
+          <td>Order Id</td>
+          <td>User Name</td>
+          <td>email</td>
+          <td>Mymensingh</td>
+          <td>Order Status </td>
+          <td>Transaction Id</td>
+          <td className="">
+            <button className="rounded btn-sm bg-black text-white flex items-center mx-auto">Invoice <span className="ps-1"><FaDownload /></span> </button>
           </td>
         </tr>
 

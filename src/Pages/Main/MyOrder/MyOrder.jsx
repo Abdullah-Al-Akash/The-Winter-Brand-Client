d88c8f4514928b58fe347c031fe29b521248e887
Abdullah Al Akash <aabdullahalakash@gmail.com>
2023-11-15 14:50:20 +0600

    update

--- src/Pages/Main/MyOrder/MyOrder.jsx
+++ src/Pages/Main/MyOrder/MyOrder.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 
 const MyOrder = () => {
-  return <div></div>;
+  return <div className="">
+    <h1 className="text-center text-xl font-extrabold p-3">
+      Your All Products
+    </h1>
+
+    <div className="overflow-x-auto">
+      <table className="table">
+        {/* head */}
+        <thead className="">
+          <tr className="">
+            <th className="text-center">#</th>
+            <th className="text-center">Product Image</th>
+            <th className="text-center">Product Name</th>
+
+            <th className="text-center ">Price</th>
+            <th className="text-center ">Discount Price</th>
+            <th className="text-center ">Available quantity</th>
+            <th className="text-center">Already Sell</th>
+            <th className="text-center">Product States</th>
+            <th className="text-center">Action</th>
+          </tr>
+        </thead>
+
+
+        <tr className="text-center">
+          <th></th>
+          <td>
+
+          </td>
+          <td>product_name </td>
+          <td>price </td>
+          <td>discount </td>
+          <td>quantity</td>
+          <td>already_sell</td>
+          <td>
+
+          </td>
+        </tr>
+
+      </table>
+    </div>
+  </div>;
 };
 
 export default MyOrder;

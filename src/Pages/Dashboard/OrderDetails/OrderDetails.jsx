588fceec7960228bf81e4f4bdca8af95262d5405
Abdullah Al Akash <aabdullahalakash@gmail.com>
2023-11-16 20:38:45 +0600

    Update

---
+++ src/Pages/Dashboard/OrderDetails/OrderDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderDetails = () => {
+    return (
+        <div>
+            <h3>Hello Order Details:</h3>
+        </div>
+    );
+};
+
+export default OrderDetails;
